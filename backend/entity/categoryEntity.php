3a2000507104bfe248af02df369787f332886907
aDargui <adargui@gmail.com>
2023-01-03 23:37:08 +0100

    Ajouts des entity

--- backend/entity/categoryEntity.php
+++ backend/entity/categoryEntity.php
@@ -1,4 +1,7 @@
 <?php
+//Faut installer l'extention Getter Setter ..
+//(Ctrl+p) .. (>) .. (Extention getter et setter ...) 
+
 
 /**
  * categoryEntity.php
@@ -6,6 +9,39 @@
  * site Web : adargui.fr
  */
 class categoryEntity{
+    // avec '?' peux prendre la valeur null
+
+    /**
+     * Identifiant de la categorie
+     */
+    protected ?int $idCategory;
+
+    /**
+     * Le nom de la categorie
+     */
+    protected string $name;
+
+    /**
+     * Getter et Setter
+     */
+    function getIdCategory() { 
+        return $this->idCategory; 
+    } 
+
+    function setIdCategory($idCategory) {  
+        $this->idCategory = $idCategory; 
+    } 
+
+    function getName() { 
+        return $this->name; 
+    } 
+
+    function setName($name) {  
+        $this->name = $name; 
+    } 
+
 }
 
+
+	
 ?>
--- backend/entity/userEntity.php
+++ backend/entity/userEntity.php
@@ -6,6 +6,7 @@
  * site Web : adargui.fr
  */
 class userEntity{
+    
 }
 
 ?>
