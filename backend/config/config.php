211d1f7620457ffbae1c37cbcc1fedd03b3e3cd1
aDargui <adargui@gmail.com>
2023-01-03 23:19:34 +0100

    Connextion a la base de donner

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,9 +1,8 @@
 <?php
     define("DB_USER","root");
-    define("DB_PASSWORD","");
+    define("DB_PASSWORD","root");
     define("HOST", "localhost");
-    define("DB_NAME", "web_service_back_front
-    ");
+    define("DB_NAME", "web_service_back_front");
 
 
 ?>
